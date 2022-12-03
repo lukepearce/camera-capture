69401994be56e7c7c210faff6b54adb5399f969a
Luke Pearce <hello@lukepearce.co.uk>
2022-12-03 23:32:29 +0000

    Remove anything that can't run on client

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  const supported = 'mediaDevices' in navigator;
-
-  console.log('supported', supported);
   const takePhoto = () => {
     const player = document.getElementById('player') as HTMLVideoElement;
     const canvas = document.getElementById('canvas')! as HTMLCanvasElement;
